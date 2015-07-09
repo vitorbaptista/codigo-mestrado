a67f19787f80e7b41b1d0e67d305dd5cd41d064f
Vitor Baptista <vitor@vitorbaptista.com>
2015-07-09 13:37:54 +0100

    Modifica período de análise na mudança de comportamento
    Ao invés de analisarmos os períodos a partir do número de votações, usamos o
    tempo: sempre um período de 365 dias divididos no meio, pegando mês a mês
    (e.x.: Fev/2003 até Fev/2004, Mar/2003 até Mar/2004, Abr/2003 até Abr/2004,
    etc.).
    
    Como os meses têm duração diferente, para evitar que o número de dias mude
    dependendo do mês inicial, somamos sempre um número fixo de dias (365). Em
    outras palavras, o período de fevereiro a fevereiro tem um número de dias
    diferente do período de março a março, por exemplo. Talvez não faça tanta
    diferença, e não valha a pena o aumento na complexidade, mas fiz assim por
    enquanto.
    
    Também, quando eu busco (por exemplo) a votação anterior a 04/Fev/2014, estou
    tratando só de dias. Se houverem duas votações no dia 03/Fev/2014, uma as 14h e
    outra as 16h, eu vou escolher ambas. O mesmo é feito quando buscando votações
    posteriores a uma data.

--- calculate-behaviour-change-using-pooles-methodology.R
+++ calculate-behaviour-change-using-pooles-methodology.R
@@ -1,4 +1,4 @@
-pkgs = c("wnominate", "pscl", "foreach", "doParallel", "data.table", "doBy")
+pkgs = c("oc", "wnominate", "pscl", "foreach", "doParallel", "data.table", "doBy")
 for (pkg in pkgs) {
   if (!(pkg %in% .packages(all.available = TRUE))) {
     install.packages(pkg)
@@ -6,7 +6,7 @@ for (pkg in pkgs) {
   library(pkg, character.only = TRUE)
 }
 
-TRIALS = 15
+TRIALS = 1
 
 minorityVoteShare <- function(votes) {
   countVotesTypes <- table(votes)
@@ -50,22 +50,27 @@ doTheThing <- function(votes, vote_metadata) {
   }
   
   mask <- rep(NA, length(votes))
-  inverse_mask <- rep(NA, length(votes))
   mask[1:column_index] <- 0
-  inverse_mask[is.na(mask)] <- 0
+
+  not_in_legislature_code = 9
+  votes_before = votes
+  votes_after = votes
+  votes_before[is.na(mask)] = not_in_legislature_code
+  votes_after[!is.na(mask)] = not_in_legislature_code
   
+  return(list(votes_before, votes_after))
   return(c(list(votes + mask), list(votes + inverse_mask)))
 }
 
 calculateOC <- function(votes, votes_metadata, baseline_party) {
   polarity <- getPolarityName(votes, baseline_party)
   
-  legis.names <- votes$name
+  legis.names <- paste(votes$id, votes$name, sep="-")
   legis.data <- votes[c('id', 'name', 'party', 'state')]
   vote.names <- votes_metadata$id
   vote.data <- votes_metadata
   votes <- votes[, -c(1:ncol(legis.data))]
-  rownames(votes) <- legis.names # paste(legis.names, legis.data$party)
+  rownames(votes) <- legis.names
   
   vote.data <- vote.data[vote.names %in% colnames(votes),]
   vote.names <- colnames(votes)
@@ -80,7 +85,7 @@ calculateOC <- function(votes, votes_metadata, baseline_party) {
                             legis.names=metadata$legis.names, legis.data=metadata$legis.data,
                             vote.names=metadata$vote.names, vote.data=metadata$vote.data)
   
-  return(wnominate(votesRollCall, dims = 1, polarity = "Amauri Teixeira",
+  return(wnominate(votesRollCall, dims = 1, polarity = "73540-José Genoíno",
                    trials = TRIALS))
 }
 
@@ -107,7 +112,7 @@ runAnalysis <- function (votes, votes_metadata, baseline_party, changed_coalitio
                                      c("id", "name", "party", "state")]
   legislatorsIds <- unique(legislators$id)
   
-  amauriTeixeira <- todosOC$legislators[todosOC$legislators$name == "Amauri Teixeira",]
+  amauriTeixeira <- todosOC$legislators[todosOC$legislators$name == "José Genoíno",]
   luizCouto <- todosOC$legislators[todosOC$legislators$name == "Luiz Couto",]
   
   result <- foreach(i = 1:length(legislatorsIds), .combine = rbind) %do% {
@@ -149,9 +154,8 @@ runAnalysis <- function (votes, votes_metadata, baseline_party, changed_coalitio
   return(result)
 }
 
-setwd("~/Projetos/Mestrado/theRealPipeline")
 baseline_party <- 'PT'
-legislature <- 54
+legislature <- 50
 csvFile <- paste(legislature, ".csv", sep="")
 metadataCsvFile <- paste(legislature, "-votacoes.csv", sep="")
 numberColumns <- length(scan(csvFile, sep=",", what="character", nlines=1))
@@ -171,28 +175,96 @@ lop <- 0.025
 votes <- votes[, c(rep(TRUE, 4), apply(votes[, -c(1:4)], 2, minorityVoteShare) > lop)]
 votes_metadata <- votes_metadata[votes_metadata$id %in% colnames(votes)[5:ncol(votes)],]
 
-cl <- makeCluster(detectCores())
+cl <- makeCluster(detectCores() - 2)
 registerDoParallel(cl)
 
 votes_metadata <- votes_metadata[order(votes_metadata$data),]
-legislators <- votes[-which(votes$name == "Amauri Teixeira"),
+legislators <- votes[-which(votes$name == "José Genoíno"),
                      c("id", "name", "party", "state")]
 
-for (vote_index in seq(100 + 4, ncol(votes), 10)) {
-  print(paste("Calculating 1~", vote_index, "...", sep=""))
+start_dates <- seq(as.POSIXct("1995-02-01"), as.POSIXct("1998-02-01"), by = "months")
+secs_in_365_days <- 365*24*60*60 # one year
+halved_secs_in_365_days <- floor(secs_in_365_days / 2)
 
-  vote_id = (colnames(votes)[[floor((vote_index - 4) / 2)]])
-  vote_metadata <- votes_metadata[votes_metadata$id == vote_id,]
+closest_vote <- function(votes_metadata, date) {
+  # Returns latest rollcall on first day with rollcalls after "date", or
+  # earliest rollcall on first day with rollcalls before "date"
+  votes_dates_diffs <- abs(trunc(votes_metadata$data, "days") - date)
+  candidates_indexes <- which(votes_dates_diffs == min(votes_dates_diffs))
+  candidates <- votes_metadata[candidates_indexes,,drop=FALSE]
+  if (candidates[1, "data"] <= date) {
+    candidates[which.min(candidates$data),]
+  } else {
+    candidates[which.max(candidates$data),]
+  }
+}
+
+closest_vote_after <- function(votes_metadata, date) {
+  # Returns latest rollcall on first day with rollcalls after "date"
+  # (including the date itself)
+  votes_dates_diffs <- trunc(votes_metadata$data, "days") - date
+  non_negative_votes_dates_diffs <- votes_dates_diffs[votes_dates_diffs >= 0]
+  if (length(non_negative_votes_dates_diffs) > 0) {
+    candidates_indexes <- which(votes_dates_diffs == min(non_negative_votes_dates_diffs))
+    candidates <- votes_metadata[candidates_indexes,,drop=FALSE]
+    candidates[which.max(candidates$data),]
+  }
+}
+
+closest_vote_before <- function(votes_metadata, date) {
+  # Returns earliest rollcall on first day with rollcalls before "date"
+  # (including the date itself)
+  votes_dates_diffs <- trunc(votes_metadata$data, "days") - date
+  non_positive_votes_dates_diffs <- votes_dates_diffs[votes_dates_diffs <= 0]
+  if (length(non_positive_votes_dates_diffs) > 0) {
+    candidates_indexes <- which(votes_dates_diffs == max(non_positive_votes_dates_diffs))
+    candidates <- votes_metadata[candidates_indexes,,drop=FALSE]
+    candidates[which.min(candidates$data),]
+  }
+}
+
+already_calculated_run_ids = c()
+foreach(start_date = start_dates, .errorhandling = "remove") %do% {
+  end_date = start_date + secs_in_365_days
+  mid_date = start_date + halved_secs_in_365_days
+
+  start_vote = closest_vote_after(votes_metadata, start_date)
+  end_vote = closest_vote_before(votes_metadata, end_date)
+  mid_vote = closest_vote(votes_metadata, mid_date)
+
+  start_vote_index = which(votes_metadata$id == start_vote$id)
+  mid_vote_index = which(votes_metadata$id == mid_vote$id)
+  end_vote_index = which(votes_metadata$id == end_vote$id)
+
+  run_id = paste(start_vote$id, end_vote$id, mid_vote$id)
+  if (run_id %in% already_calculated_run_ids) {
+    stop("Skipping loop already calculated.");
+  } else {
+    already_calculated_run_ids = c(already_calculated_run_ids, run_id)
+  }
+  print(paste("Calculating", start_date, "until", end_date, "split on", mid_date, "..."))
+  start_time = proc.time()
 
   result <- foreach(i = seq(1, nrow(legislators), 1),
                     .inorder = FALSE,
-                    .packages = c('wnominate', 'foreach', 'data.table')) %dopar% {
+                    .errorhandling = "remove",
+                    .packages = c("wnominate", "foreach", "data.table")) %do% {
+    set.seed(1)
     legislator <- legislators[i,]
-    votes_split <- splitLegislatorOnVote(votes[votes$id == legislator$id, 1:vote_index],
-                                         vote_metadata,
+    # As 4 primeiras colunas são metadados dos parlamentares, e as outras são
+    # as votações. Por isso adicionamos esse offset
+    columns <- c(1:4, (start_vote_index+4):(end_vote_index+4))
+    votes_split <- splitLegislatorOnVote(votes[votes$id == legislator$id, columns],
+                                         mid_vote,
                                          votes)
     calculateOC(votes_split, votes_metadata, baseline_party)
   }
- 
-  saveRDS(result, file = paste(legislature, "-1_", vote_index, ".rds", sep=""))
+
+  print("# Processing time:")
+  print(proc.time() - start_time)
+
+  saveRDS(result, file = paste(legislature, "-", TRIALS, "-", start_vote$id, "_", end_vote$id, ".rds", sep=""))
+
+  print("# Writing time:")
+  print(proc.time() - start_time)
 }
