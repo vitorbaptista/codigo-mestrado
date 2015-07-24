de40885d03e6f056c903da33397060149c0fc2f6
Vitor Baptista <vitor@vitorbaptista.com>
2015-07-24 18:19:04 +0100

    Desabilita debugging do SQLAlchemy

--- pipeline/db.py
+++ pipeline/db.py
@@ -9,8 +9,7 @@ from sqlalchemy.orm import sessionmaker
 DATA_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                          os.path.pardir,
                          'data')
-engine = sqlalchemy.create_engine('sqlite:///%s/dados.db' % DATA_PATH,
-                                  echo = True)
+engine = sqlalchemy.create_engine('sqlite:///%s/dados.db' % DATA_PATH)
 engine.raw_connection().connection.text_factory = str
 Session = sessionmaker(bind=engine)
 session = Session()
